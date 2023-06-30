8f976a7be1da04e7d008c9b237032c532a0696e9
chasura416 <chasura416@naver.com>
2023-06-30 11:36:08 +0900

    useReducer 공부

--- react/230630/recap/src/components/hide_timer.js
+++ react/230630/recap/src/components/hide_timer.js
@@ -4,16 +4,21 @@ import Timer from './timer';
 
 
 const HideTimer = () => {
-  const [hide, setHide] = useState(false);
+  const [hide, setHide] = useState(true);
 
   const ResetBtn = () => {
     setHide(!hide);
   }
 
+  // Timer 컴포넌트를 가져다가 사용하는 방법을 생각못하고 여기서 아예 새로 만들어서
+  // 구현을 하려다 보니 시간의 초기화가 안됨. 가져다가 쓰니 바로 된다. 
+  // 리액트의 lifecycle을 파악하면 이해가 쉽다. 제대로 다시 검색해서 숙지하자.
   return(
     <div>
       {hide && <Timer />}
-      <button onClick={ResetBtn}>Hide Timer</button>
+      <button onClick={ResetBtn}>
+        {hide ? "Hide Timer" : "Show Timer"}
+      </button>
     </div>
   );
 }
