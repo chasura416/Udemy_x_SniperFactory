466501f7426ddb51d36a5a3a64589af91a38a81c
chasura416 <chasura416@naver.com>
2023-07-05 18:01:55 +0900

    API_KEY 설정

--- react/230705/practice-firebase/src/fConfig.js
+++ react/230705/practice-firebase/src/fConfig.js
@@ -6,16 +6,15 @@ import {
   onAuthStateChanged,
 } from "firebase/auth";
 
-const FIREBASE_API_KEY = process.env.REACT_APP_FIREBASE_API_KEY;
-
+// API_KEY는 항상 신경써서 관리하자. 
 const firebaseConfig = {
-  apiKey: FIREBASE_API_KEY,
-  authDomain: "practice-project-5f967.firebaseapp.com",
-  projectId: "practice-project-5f967",
-  storageBucket: "practice-project-5f967.appspot.com",
-  messagingSenderId: "473579289999",
-  appId: "1:473579289999:web:5037e5c9815c68bd9edffa",
-  // measurementId: "G-SK53LRMYRJ"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  // measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
