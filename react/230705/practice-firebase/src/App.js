43e246a9fc50c6e822232fc9c30753849fc2a5eb
chasura416 <chasura416@naver.com>
2023-07-05 13:01:59 +0900

    firebase 초기세팅

--- react/230705/practice-firebase/src/App.js
+++ react/230705/practice-firebase/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
 function App() {
+  console.log(process.env.REACT_APP_FIREBASE_API_KEY);
+  console.log(1+2);
   return (
     <div className="App">
       파이어베이스 시작화면
---
+++ react/230705/practice-firebase/src/fConfig.js
@@ -0,0 +1,24 @@
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+
+const FIREBASE_API_KEY = process.env.REACT_APP_FIREBASE_API_KEY;
+
+const firebaseConfig = {
+  apiKey: FIREBASE_API_KEY,
+  authDomain: "practice-project-5f967.firebaseapp.com",
+  projectId: "practice-project-5f967",
+  storageBucket: "practice-project-5f967.appspot.com",
+  messagingSenderId: "473579289999",
+  appId: "1:473579289999:web:5037e5c9815c68bd9edffa",
+  // measurementId: "G-SK53LRMYRJ"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { createUserWithEmailAndPassword, signInWithEmailAndPassword };
+export default auth;
